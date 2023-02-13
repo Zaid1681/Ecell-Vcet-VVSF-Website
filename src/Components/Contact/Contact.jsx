0435a4a64cbe14289a2763001a907dbf47fe2e27
anishDalvi <anishdalvi2001@gmail.com>
2023-02-14 01:04:31 +0530

    map added

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -35,6 +35,10 @@ const Contact = () => {
                             <p className="card-text"> <b>ecell@vcet.edu.in</b> </p>
                     </div>
                 </div>
+                <div className="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3763.6598743525196!2d72.82654491531592!3d19.3838746472589!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7aec0a4b41bef%3A0xbd1a4ca919d6a613!2sVidyavardhini&#39;s%20College%20of%20Engineering%20and%20Technology!5e0!3m2!1sen!2sin!4v1676316549934!5m2!1sen!2sin"  style={{border:0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    
+                </div>
             </div>
         </div>
 
